6d5fa33f02b50ebb01b7f7035298cd87db5311cf
victordelval <victor.val.andres@gmail.com>
2018-02-02 00:45:02 +0100

    Improves map component (components, styles, resize, etc.)

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ReactMapGL, {Marker, Popup} from 'react-map-gl';
-// import ReactMapGL, {Marker} from 'react-map-gl';
+
+import MapGL, {Marker, Popup, NavigationControl} from 'react-map-gl';
+
+import PlacePin from '../PlacePin';
+import PlaceInfo from '../PlaceInfo';
+
+
+const navStyle = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    padding: '10px'
+  };
+
 
 class Map extends React.Component {
 
@@ -13,38 +25,91 @@ class Map extends React.Component {
         }).isRequired
     }
 
-    state = {
-        viewport: {
-            width: 800,
-            height: 400,
-            // latitude: 37.7577,
-            // longitude: -122.4376,
-            latitude: parseFloat(this.props.coordinates.latitude),
-            longitude: parseFloat(this.props.coordinates.longitude),
-            zoom: 13
-        }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewport: {
+                latitude: parseFloat(this.props.coordinates.latitude),
+                longitude: parseFloat(this.props.coordinates.longitude),
+                zoom: 11,
+                bearing: 0,
+                pitch: 0,
+                width: 500,
+                height: 300,
+            },
+            popupInfo: null
+        };
+    }
+
+
+    componentDidMount() {
+        window.addEventListener('resize', this._resize);
+        this._resize();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this._resize);
+    }
+
+    _resize = () => {
+        var x = document.getElementsByTagName("MAIN")[0];
+        this.setState({
+            viewport: {
+                ...this.state.viewport,
+                width: this.props.width || x.offsetWidth,
+            }
+        });
     };
 
-    render() {
-        console.log(">> map")
-        console.log(this.props)
-        return (
-            <ReactMapGL
-                {...this.state.viewport}
-                onViewportChange={(viewport) => this.setState({ viewport })}
-                mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}>
+    _updateViewport = (viewport) => {
+        this.setState({ viewport });
+    }
 
-                    { this.props.data.map(location =>
-                        <Marker latitude={parseFloat(location.lat)} longitude={parseFloat(location.lng)} >
-                            <div>{ location.name }</div>
-                        </Marker>
-                        // <Popup latitude={ parseFloat(location.lat) } longitude={ parseFloat(location.lng) } closeButton={true} closeOnClick={true} anchor="top">
-                        //     <div>{ location.name }</div>
-                        // </Popup>
-                    )}
-            </ReactMapGL>
+    _renderPlaceMarker = (place, index) => {
+        return (
+            <Marker key={`marker-${index}`}
+                longitude={parseFloat(place.lng)}
+                latitude={parseFloat(place.lat)} >
+                <PlacePin size={20} onClick={() => this.setState({ popupInfo: place })} />
+            </Marker>
         );
     }
+
+    _renderPopup() {
+        const { popupInfo } = this.state;
+
+        return popupInfo && (
+            <Popup tipSize={5}
+                anchor="top"
+                longitude={parseFloat(popupInfo.lng)}
+                latitude={parseFloat(popupInfo.lat)}
+                onClose={() => this.setState({ popupInfo: null })} >
+                <PlaceInfo info={popupInfo} />
+            </Popup>
+        );
+    }
+
+
+    render() {
+        const { viewport } = this.state;
+        return (
+            <MapGL
+                {...viewport}
+                mapStyle="mapbox://styles/mapbox/dark-v9"
+                onViewportChange={this._updateViewport}
+                mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} >
+
+                {this.props.data.map(this._renderPlaceMarker)}
+                {this._renderPopup()}
+                <div className="nav" style={navStyle}>
+                    <NavigationControl onViewportChange={this._updateViewport} />
+                </div>
+                {/* <ControlPanel containerComponent={this.props.containerComponent} /> */}
+            </MapGL>
+        );
+    }
+
 }
 
 export default Map;
