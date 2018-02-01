7c311260866d34034d31916df7fb6e595e86c1dd
victordelval <victor.val.andres@gmail.com>
2018-02-01 10:10:20 +0100

    Fixes PropTypes warnings due to separate package now

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 
 /**
  * Renderiza el formulario de búsqueda.
--- src/containers/SearchContainer/SearchContainer.js
+++ src/containers/SearchContainer/SearchContainer.js
@@ -1,4 +1,3 @@
-// import React, { PropTypes } from 'react';
 import React from 'react';
 
 // Actions
